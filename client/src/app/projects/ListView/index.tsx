a6ce09291d0f0774ee137301927dfeab60ac4c45
“Nghiem <nghiemlinh10d3@gmail.com>
2026-03-18 22:49:08 +0700

    Create ListView

---
+++ client/src/app/projects/ListView/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { useGetTasksQuery } from '@/state/api';
+import Header from '@/components/Header';
+import TaskCard from '@/components/TaskCard';
+type Props = {
+    id: string;
+  setIsModalNewTaskOpen: (isOpen: boolean) => void;
+}
+
+const ListView = ({ id, setIsModalNewTaskOpen }: Props) => {
+    const { data: tasks, isLoading, error } = useGetTasksQuery({ projectId: Number(id) });
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>An error occurred while fetching tasks</div>;
+  return(
+    <div className="px-4 pb-8 xl:px-6">
+        <div className="pt-5">
+            <Header name="List"/>
+        </div>
+        <div className="grid grid-cols1 gap4 md:grid-cols-2 lg:grid-cols-3 lg:gap-6">
+        {tasks?.map((task) => (
+          <TaskCard key={task.id} task={task} />
+        ))}
+        </div>
+    </div>
+  )
+}
+
+export default ListView
