24b08ffe9e693b32b02541d9b10c346ed278d19d
“Nghiem <nghiemlinh10d3@gmail.com>
2026-03-18 23:21:49 +0700

    TableView feature

--- client/src/app/projects/[id]/page.tsx
+++ client/src/app/projects/[id]/page.tsx
@@ -4,6 +4,7 @@ import ProjectHeader from '../ProjectHeader';
 import Board from '../BoardView';
 import List from '../ListView';
 import Timeline from '../TimelineView';
+import Table from '../TableView';
 
 type Props = {
   params: Promise<{ id: string }>;
@@ -24,6 +25,9 @@ const Project = ({ params }: Props) => {
       )}
       {activeTab === "Timeline" && (
         <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Table" && (
+        <Table id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
       )}
     </div>
   )
